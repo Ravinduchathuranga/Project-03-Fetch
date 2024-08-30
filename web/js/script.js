f8ed39dba62a9ca3750d74d1fd1b87c3153db9c0
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2024-08-30 10:15:13 +0530

    fetch updated!

--- web/js/script.js
+++ web/js/script.js
@@ -1,7 +1,14 @@
 function getRequest() {
-    fetch("X?name=ravindu").then(
-    Response => {
-        console.log("Response done");
-    }
-);
+    fetch("X?name=ravindu")
+        .then(
+            Response => {
+                console.log("Response done");
+                return Response.text();
+            }
+        )
+        .then(
+            text => {
+                console.log(text);
+            }
+        );
 }
